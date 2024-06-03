8abef56a5e94d66a26edc98c9038621ec9b66b56
clecio <clecio.lord@gmail.com>
2024-06-03 09:36:35 -0500

    feat: func para plotar imagens do jmuben nao testado

--- util/plots.py
+++ util/plots.py
@@ -45,3 +45,40 @@ class Plots:
         plt.ylabel("Dados", fontsize=15)
         plt.savefig(out)
         print('BRACOL images graph saved in: '+out)
+
+
+    def plot_jmuben_images(self):
+        if not os.path.exists("./output/imagesGraph"):
+            os.makedirs("./output/imagesGraph")
+        out = './output/imagesGraph/'
+
+        data_folder = '/data/dataimages/dataset_original_aumentado'
+        healthy2 = len(os.listdir(data_folder +'/Healthy'))
+        phome2 = len(os.listdir(data_folder +'/Phoma'))
+        miner2 = len(os.listdir(data_folder +'/Miner'))
+        rust2 =  len(os.listdir(data_folder +'/Leaf rust'))
+        cercospora2 = len(os.listdir(data_folder +'/Cerscospora'))
+        total = healthy2 + phome2 + miner2 + rust2 + cercospora2
+
+        labels = [healthy2, phome2, miner2 ,rust2 ,cercospora2]
+
+        index = ['Healthy', 'Phome', 'Miner', 'Rust', 'Cercospora']
+
+        print('Total Images =', total)
+
+        df = pd.DataFrame({'Quantidade': labels}, index=index)
+
+        fig, ax = plt.subplots(figsize=(7, 6) , dpi = 150)  # Ajuste os valores de largura e altura conforme necessário
+        ax = df.plot.bar(rot=0, color={'Quantidade': "green"}, ax=ax)
+
+        # Define manualmente os limites do eixo x
+        ax.set_xlim(-0.6, len(index) - 0.2)
+        plt.xticks(range(len(index)), index, fontsize=16)
+
+        ax.set_ylim(0, max(labels) + 300)
+
+        for container in ax.containers:
+            ax.bar_label(container)
+        plt.ylabel("Dados", fontsize=16)
+        plt.show(out)
+        print('JMUBEN images graph saved in: '+out)
